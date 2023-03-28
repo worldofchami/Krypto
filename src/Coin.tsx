0ed29c0167a0756d23fa7616970ece1523462a7e
TINO_CHAMI <TCHAMINUKA19@GMAIL.COM>
2023-03-28 18:31:52 +0200

    Index page

---
+++ src/Coin.tsx
@@ -0,0 +1,11 @@
+import { useParams } from "react-router-dom";
+
+export const Coin: React.FunctionComponent<{}> = () => {
+    const { id } = useParams();
+    
+    return (
+        <>
+
+        </>
+    );
+};
