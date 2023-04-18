70eb1b47eb4c1c5a943dae5f028ea3302d79e40e
TINO_CHAMI <TCHAMINUKA19@GMAIL.COM>
2023-04-18 13:11:41 +0200

    Adding aliases

--- server/app.ts
+++ server/app.ts
@@ -2,7 +2,7 @@ import express from "express";
 const app = express();
 import dotenv from "dotenv";
 dotenv.config();
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
     console.log("listening", port);
