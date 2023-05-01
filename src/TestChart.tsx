16c5659075c119fff98140d74a676e4b5d77bef6
TINO_CHAMI <TCHAMINUKA19@GMAIL.COM>
2023-05-01 22:03:28 +0200

    Customising chart tooltip

--- src/TestChart.tsx
+++ src/TestChart.tsx
@@ -1,13 +1,118 @@
+import { Line } from "react-chartjs-2";
 import { ChartData } from "chart.js";
+import { ChartOptions } from "chart.js";
+import { useState } from "react";
+import { TooltipModel, Chart } from "chart.js";
 
-const Chart = () => {
+interface Tooltip {
+    opacity: number;
+    top: number;
+    left: number;
+    right: number;
+    date: string;
+    value: string;
+}
 
+const MSChart = () => {
+    const [tooltip, setTooltip] = useState<Tooltip>({
+        opacity: 0,
+        top: 0,
+        left: 0,
+        right: 0,
+        date: "",
+        value: ""
+    });
+
+    /*
+    const chartOptions: ChartOptions<"line"> = {
+        plugins: {
+            legend: {
+                display: false,
+            },
+            tooltip: {
+                boxHeight: 100,
+                boxWidth: 100,
+                mode: ('index' as 'index'),
+                intersect: false,
+                displayColors: false,
+                callbacks: {
+                    title: () => "Bitcoin",
+                    footer: (tooltipItem: any) => "$" + tooltipItem[0].raw,
+                    label: (tooltipItem: any) => tooltipItem.label,
+                }
+            }
+        },
+        scales: {
+            y: {},
+            x: {},
+        },
+        elements: {
+            point: {
+                radius: 0,
+            },
+        },
+    };
+    */
+
+    const generateTooltip = (tooltipModel: TooltipModel<"line">, chart: Chart) => {
+        if(tooltipModel.opacity !== 0) {
+            setTooltip((prev) => ({ ...prev, opacity: 0 }));
+        }
+
+        const position = chart.canvas.getBoundingClientRect();
+    };
+
+    const chartOptions: ChartOptions<"line"> = {
+        plugins: {
+            legend: {
+                display: false,
+            },
+            tooltip: {
+                enabled: false,
+                external: ({ tooltip: tooltipModel, chart }): void => generateTooltip(tooltipModel, chart),
+            }
+        },
+        scales: {
+            y: {},
+            x: {},
+        },
+        elements: {
+            point: {
+                radius: 0,
+            },
+        },
+    };
+
+    const data: number[] = [0,1,2,3,4,5,6,7,1,2,1,8,21,21,12,3];
+
+    const labels: string[] = [];
+    for (const idx in data) {
+        labels.push("May " + (idx+1));
+    }
+
+    const chartData: ChartData<"line"> = {
+        labels,
+        datasets: [
+            {
+                data,
+                borderColor: data[0] > data[-1] ? "#00ff90" : "#fe1b30",
+                stepped: false,
+            },
+        ],
+    }
+
+    return <Line data={chartData} options={chartOptions} />;
+}
+
+interface TTModel {
+    chart: Chart;
+    tooltip: TooltipModel<"line">;
 }
 
 export const TestChart = () => {
     return (
-        <>
-        
-        </>
+        <div className="w-full h-72">
+            <MSChart />
+        </div>
     );
 }
